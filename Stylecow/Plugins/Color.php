6f21e02e4fd0edab93cb068517cb42e702918a93
Óscar Otero <oom@oscarotero.com>
2012-06-14 20:10:59 +0200

    Fixed Color plugin in strict mode

--- Stylecow/Plugins/Color.php
+++ Stylecow/Plugins/Color.php
@@ -13,7 +13,7 @@
  *
  * @author Oscar Otero <http://oscarotero.com> <oom@oscarotero.com>
  * @license GNU Affero GPL version 3. http://www.gnu.org/licenses/agpl-3.0.html
- * @version 0.4.3 (2012)
+ * @version 0.4.4 (2012)
  */
 
 namespace Stylecow;
@@ -232,7 +232,9 @@ class Color implements Plugins_interface {
 	 * @return array  The transformed color
 	 */
 	private function colorCallback ($matches) {
-		list($color, $operations) = $this->Css->explodeTrim(',', $matches[1], 2);
+		$matches = $this->Css->explodeTrim(',', $matches[1], 2);
+		$color = $matches[0];
+		$operations = isset($matches[1]) ? $matches[1] : '';
 
 		$rgba = $this->toRGBA($color);
 

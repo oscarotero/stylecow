429be554730fb1b4abb1a05154e47acb1c425d26
Oscar Otero <oom@oscarotero.com>
2013-05-16 22:01:49 +0200

    Added an autoloader file for non composer installations

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
+include('Stylecow/autoloader.php');
+
 use Stylecow\Parser;
-
-//A simple PSR-0 autoload function
-function autoload ($className) {
-	$className = ltrim($className, '\\');
-	$fileName  = '';
-	$namespace = '';
-	
-	if ($lastNsPos = strripos($className, '\\')) {
-		$namespace = substr($className, 0, $lastNsPos);
-		$className = substr($className, $lastNsPos + 1);
-		$fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-	}
-
-	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-	if (is_file($fileName)) {
-		require $fileName;
-	}
-}
-
-spl_autoload_register('autoload');
 
 $plugins = array(
 	'Color' => array(
